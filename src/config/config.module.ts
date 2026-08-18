4c9a9abeb4af2cfbb85e99e9d31fd4b3f6fc29a6
Yousef Dawood <yousefdawood31@gmail.com>
2026-08-18 12:24:49 +0300

    feat: add PrismaService and update configuration for database integration

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -2,15 +2,22 @@ import { Global, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { env } from '../utils/env.js';
 import { KEYS } from '../core/keys.js';
 import helmet from 'helmet';
+import { PrismaService } from '../database/prisma.service.js';
 
 @Global()
 @Module({
   providers: [
     {
+      provide: PrismaService,
+      useClass: PrismaService,
+    },
+    {
       provide: KEYS.ENV,
       useValue: env,
     },
   ],
+
+  exports: [KEYS.ENV, PrismaService],
 })
 export class ConfigModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
